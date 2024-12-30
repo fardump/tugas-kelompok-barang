7ddaff62ba5d01f579e2f50e7740f6121bb2881c
fardump <farkhanfirmans@gmail.com>
2024-12-30 21:48:59 +0700

    2nd Commit Testing branch

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h2>Home Page</h2>
+    </div>
+    <div class="content">
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed ipsum, dolorum doloribus dignissimos illum numquam atque facilis molestiae provident quibusdam modi suscipit sit laudantium debitis asperiores eos aut doloremque libero!
+    </div>
+</body>
+</html>
